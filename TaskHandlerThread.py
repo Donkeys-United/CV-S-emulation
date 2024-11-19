79536990071083b1d198787ff6a91e728c406b1c
Tobi7552 <60338540+Tobi7552@users.noreply.github.com>
2024-11-19 12:57:18 +0100

    Create TaskHandlerThread.py
    Initial sketch af TaskHandlerThread

---
+++ TaskHandlerThread.py
@@ -0,0 +1,43 @@
+#Libraries
+
+
+class TaskHandlerThread():
+
+
+
+    #Attributes:
+    allocatedTasks = []
+    unallocatedTasks = []
+
+
+    def run():
+        pass
+
+    def doAllocateTaskToSelf(task):
+        pass
+
+
+    def sendRequest():
+        pass
+
+    def sendRespond():
+        pass
+
+    def sendDataPacket():
+        pass
+
+    def changeFrequency(frequency):
+        pass
+
+    def getAcceptedTaskTotal():
+        pass
+
+    def enqueueUnallocatedTask(task):
+        pass
+
+
+
+
+
+
+
