72830f7d30b7c772b8282b23260006f94a5ecde3
Tobi7552 <60338540+Tobi7552@users.noreply.github.com>
2024-11-19 13:58:58 +0100

    Update TaskHandlerThread.py
    More sketching of the class

--- TaskHandlerThread.py
+++ TaskHandlerThread.py
@@ -1,43 +1,50 @@
 #Libraries
+import threading
+from Task import Task
+from OribtalPositionThread import canExecuteMission
 
 
-class TaskHandlerThread():
-
-
+class TaskHandlerThread(threading.Thread):
 
     #Attributes:
-    allocatedTasks = []
-    unallocatedTasks = []
+    __allocatedTasks = []
+    __unallocatedTasks = []
+
+    def __init__(self, name, delay):
+        super().__init__()
+        self.name = name
+        self.delay = delay
+        self.running = True
+
+    def run(self,):
+        while self.running:
+            pass
 
 
-    def run():
-        pass
-
-    def doAllocateTaskToSelf(task):
-        pass
-
-
-    def sendRequest():
-        pass
-
-    def sendRespond():
-        pass
-
-    def sendDataPacket():
-        pass
-
-    def changeFrequency(frequency):
-        pass
-
-    def getAcceptedTaskTotal():
-        pass
-
-    def enqueueUnallocatedTask(task):
+    def doAllocateTaskToSelf(self, task: Task):
         pass
 
 
+    def sendRequest(self,):
+        pass
 
+    def sendRespond(self,):
+        pass
 
+    def sendDataPacket(self,):
+        pass
+
+    def changeFrequency(self, frequency: int):
+        pass
+
+    def getAcceptedTaskTotal(self, ):
+        pass
+
+    def enqueueUnallocatedTask(self, task: Task):
+        pass
+
+thread = TaskHandlerThread(name="mainWorker", delay=2)
+thread.start()
 
 
 
