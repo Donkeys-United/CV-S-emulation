021d9215a3270ccbb2289629f26fd2ad1ae53151
Engbjerg <nicolaiengbjerg@gmail.com>
2024-11-19 15:02:20 +0100

    added methods for further development

--- CommunicationThread.py
+++ CommunicationThread.py
@@ -1,13 +1,44 @@
+from collections.abc import Callable
 from threading import Thread
-from MessageClasses import Message
-from typing import List
+from MessageClasses import RequestMessage, RespondMessage, ImageDataMessage, ResponseNackMessage, ProcessedDataMessage
+from AcceptedRequestQueue import AcceptedRequestQueue
+from typing import Any, Iterable, List, Mapping
 
 class CommunicationThread(Thread):
-    unAcceptedRequests:List[Message] = []
-    transmissionQueue:List[Message] = []
-    messageList:List[Message] = []
+    unAcceptedRequests:List[RequestMessage] = []
+    transmissionQueue:List[RequestMessage | RespondMessage | ImageDataMessage | ResponseNackMessage | ProcessedDataMessage] = []
+    messageList:List[RequestMessage | RespondMessage | ImageDataMessage | ResponseNackMessage | ProcessedDataMessage] = []
+    AcceptedRequests:AcceptedRequestQueue
+
+    def __init__(
+            self,
+            group: None = None, target: Callable[..., object] | None = None, name: str | None = None,
+            args: Iterable[Any] = ..., kwargs: Mapping[str, Any] | None = None,
+            *,
+            daemon: bool | None = None
+            ) -> None:
+        super().__init__(group, target, name, args, kwargs, daemon=daemon)
+        self.AcceptedRequests = AcceptedRequestQueue()
+        self.AcceptedRequests.start()
 
     def run(self) -> None:
         return super().run()
     
-    
+    def messageTypeHandle(
+            self,
+            message: RequestMessage | ImageDataMessage | RespondMessage | ResponseNackMessage | ProcessedDataMessage
+            ) -> None:
+        
+        if type(message) == RequestMessage:
+            pass #Insert allocation function
+        elif type(message) == RespondMessage:
+            pass
+        elif type(message) == ImageDataMessage:
+            pass
+        elif type(message) == ResponseNackMessage:
+            if message.getTaskID() in self.AcceptedRequests.getIDInQueue():
+                self.AcceptedRequests.removeMessage(message.getTaskID())
+            else:
+                pass
+        elif type(message) == ProcessedDataMessage:
+            pass
