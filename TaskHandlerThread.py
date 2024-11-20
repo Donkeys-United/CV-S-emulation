7cb479e7410fbac56826bdb332d624d3c370020b
Tobi7552 <60338540+Tobi7552@users.noreply.github.com>
2024-11-20 10:37:31 +0100

    Update TaskHandlerThread.py
    Some small changes

--- TaskHandlerThread.py
+++ TaskHandlerThread.py
@@ -1,7 +1,6 @@
 #Libraries
 import threading
 from Task import Task
-from OribtalPositionThread import canExecuteMission
 
 
 class TaskHandlerThread(threading.Thread):
@@ -22,12 +21,13 @@ class TaskHandlerThread(threading.Thread):
         while self.running:
             pass
 
-    def doAllocateTaskToSelf(self, task: Task, __unallocatedTasks: list, __allocatedTasks: list):
+    def allocateTaskToSelf(self, task: Task, __unallocatedTasks: list, __allocatedTasks: list):
         """
         Method used to either allocate a task to a satellite itself, or send a request message to another satellite
         """
         if __unallocatedTasks != None:
-            x = OribtalPositionThread.canExecuteMission() #er lidt i tvivl om syntaxen er rigtig, men ellers fiks senere
+            task = __unallocatedTasks[0]
+            x = "Insert Kristian Meth"
             if x == True:
                 self.__allocatedTasks.append[task]
             else:
@@ -37,25 +37,40 @@ class TaskHandlerThread(threading.Thread):
         
 
     def sendRequest(self,):
+        """
+        Method to send a request to other satellites in order to find out if they are able to perform the task
+        """
         pass
             
 
     def sendRespond(self,):
+        """
+        Method to send a respond to other satellites telling them they can perform the requested task
+        """
         pass
 
     def sendDataPacket(self,):
+        """
+        Send task packet to 
+        """
         pass
 
     def changeFrequency(self, frequency: int):
+        """
+        Method to adjust the GPU frequency - maybe declared in another class, not this one. TBD
+        """
         pass
 
     def getAcceptedTaskTotal(self, ):
+        """
+        Method to get the ammount of accepted tasks a satellite has
+        """
         pass
 
     def enqueueUnallocatedTask(self, task: Task):
         pass
 
-thread = TaskHandlerThread(name="mainWorker", delay=2)
+thread = TaskHandlerThread(name = "mainWorker", delay = 2)
 thread.start()
 
 
