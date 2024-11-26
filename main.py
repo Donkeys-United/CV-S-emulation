24ea45b607cbbc5c39b4379be23929045b3e69ac
Musvon2705 <Musvon2705@gmail.com>
2024-11-26 13:17:53 +0100

    Initial setup of main.py

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-import CommunicationThread, TaskHandlerThread, AcceptedRequestQueue, MissionThread, MessageClasses
+import CommunicationThread, TaskHandlerThread, MissionThread, Task
+from uuid import getnode
 
+satelliteID = getnode()
+
+communicationThread = CommunicationThread.CommunicationThread()
+
+taskHandlerThread = TaskHandlerThread.TaskHandlerThread()
+
+missionThread = MissionThread.MissionThread()
+
+
+task_1 = Task.Task(satelliteID, 1, 10)
