dbf4fe6da0c81f7f69d69eb1f3671ded55a9ec58
Kristian Gotfredsen <ksg2001@live.com>
2024-12-01 20:05:56 +0100

    Changed priorityqueue to contain a frequency

--- PriorityQueue.py
+++ PriorityQueue.py
@@ -8,9 +8,9 @@ class PriorityQueue:
         None:
     
     """
-    __queue:List[Task] = []
+    __queue:List[list[Task, float]] = []
 
-    def addTaskToQueue(self, task:Task) -> None:
+    def addTaskToQueue(self, task:Task, frequency: float = 0.0) -> None:
         """Method that adds task to queue
 
         Args:
@@ -20,7 +20,7 @@ class PriorityQueue:
             None:
         
         """
-        self.__queue.append(task)
+        self.__queue.append([task, frequency])
     
     def isEmpty(self) -> bool:
         """Method that checks if queue is empty
@@ -37,7 +37,7 @@ class PriorityQueue:
         else:
             return False
 
-    def nextTask(self) -> Optional[Task]:
+    def nextTask(self) -> Optional[list[Task, float]]:
         """Method for getting the next task in the queue. Method removes the task retrieved
 
         Args:
@@ -57,3 +57,19 @@ class PriorityQueue:
             return next_task
         else:
             return None
+    
+    def getSortedQueueList(self) -> List[list[Task, float]]:
+        return sorted(self.__queue, key=lambda task: task[0].getUnixTimestampLimit())
+
+
+if __name__ == "__main__":
+    from random import randint
+    queue = PriorityQueue()
+
+    for i in range(50):
+        queue.addTaskToQueue(Task(1,i, randint(1,20)))
+    
+    sorted_queue = queue.getSortedQueueList()
+    
+    for i in sorted_queue:
+        print(i[0].getUnixTimestampLimit(), i[1])
