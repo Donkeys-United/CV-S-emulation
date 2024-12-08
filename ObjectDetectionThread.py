1e2837dfb4ff4e049ff31af5e87c633e48cd5cf9
Musvon2705 <musvon2705@gmail.com>
2024-12-08 14:55:44 +0100

    firsthop solution perhaps

--- ObjectDetectionThread.py
+++ ObjectDetectionThread.py
@@ -94,16 +94,19 @@ class ObjectDetectionThread(threading.Thread):
             priority_list = self.communicationThread.orbitalPositionThread.getSatellitePriorityList()
             print(f"Priority List = {priority_list}")
             print(f"connections = {self.communicationThread.connections}")
+            break_out = False
             for i in range(len(priority_list)):
                 for j in self.communicationThread.connections:
                     print(f"\ni={priority_list[i]}\nj={j}")
                     if priority_list[i] == j:
                         firstHopID = j
                         print(f"\nfirstHopID = {j}")
+                        break_out = True
                         break
                     else:
                         print("\nNo First Hop!")
-                break
+                if break_out:
+                    break
 
 
 
