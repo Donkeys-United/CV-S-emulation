dda395767c82f922dceb108e8c7deb8e54506c31
Musvon2705 <musvon2705@gmail.com>
2024-12-09 13:57:54 +0100

    More windows fixes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from CommunicationThread import CommunicationThread
 from TaskHandlerThread import TaskHandlerThread
 from MissionThread import MissionThread
-from ObjectDetectionThread import ObjectDetectionThread
 from OrbitalPositionThread import OrbitalPositionThread
 from getmac import get_mac_address
 from pathlib import Path
@@ -10,6 +9,10 @@ import json
 
 satelliteID = int(get_mac_address().replace(":",""),16)
 print(satelliteID)
+
+if satelliteID != 273270825686681:
+    from ObjectDetectionThread import ObjectDetectionThread
+
 current_dir = Path(__file__).parent.resolve()
 cv_model_path = current_dir / "models" / "yolov8m_best.engine"
 image_path = current_dir / "images"
