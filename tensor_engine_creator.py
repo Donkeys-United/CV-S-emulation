0129cefb3ddacd552bca86bd42005e378b98f495
Musvon2705 <Musvon2705@gmail.com>
2024-12-11 10:29:37 +0100

    Created script for tensor engine generation

---
+++ tensor_engine_creator.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+from pathlib import Path
+
+current_dir = Path(__file__).parent.resolve()
+pt_model_path = current_dir / "models" / "yolov8m_best.pt"
+
+model = YOLO(pt_model_path)
+model.export("engine")
