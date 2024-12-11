42bd9b420e02d6093274970ae1e1b47ee7ae3d2b
Musvon2705 <Musvon2705@gmail.com>
2024-12-11 10:34:44 +0100

    Fixed tensor script

--- tensor_engine_creator.py
+++ tensor_engine_creator.py
@@ -5,4 +5,4 @@ current_dir = Path(__file__).parent.resolve()
 pt_model_path = current_dir / "models" / "yolov8m_best.pt"
 
 model = YOLO(pt_model_path)
-model.export("engine")
+model.export(format="engine")
